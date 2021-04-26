6820ecadd6ffdc723c35c23d8cb6ee5b83c8c801
Gregg Wong <griekup@live.com>
2021-04-26 14:10:02 -0500

    1. The Hero Editor

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,31 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Home</router-link>
+  
   </div>
+  <h1>{{ title }}</h1>
   <router-view/>
 </template>
 
 <style>
+h1 {
+  color: #369;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 250%;
+}
+h2, h3 {
+  color: #444;
+  font-family: Arial, Helvetica, sans-serif;
+  font-weight: lighter;
+}
+body {
+  margin: 2em;
+}
+body, input[type="text"], button {
+  color: #333;
+  font-family: Cambria, Georgia, serif;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -28,3 +47,14 @@
   color: #42b983;
 }
 </style>
+
+<script lang="ts">
+import {defineComponent, ref } from 'vue'
+export default defineComponent({
+  name: 'App',
+  setup(){
+    let title = ref("Tour of Heroes");  
+    return { title }  
+  }
+})
+</script>
---
+++ src/components/Heroes.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h2>{{ capitalHeroName }} Details</h2>
+        <div><span>id: </span>{{hero.id}}</div>
+        <div>
+            <label for="name">Hero name: </label>
+            <input type="text" placeholder="name" v-model="hero.name" id="name">
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
+
+<script lang="ts">
+import {computed, defineComponent, PropType} from 'vue'
+import { Hero } from '@/types/hero'
+export default defineComponent({
+    props: {
+        hero: {
+            type: Object as PropType<Hero>,
+            default: { id: 1, name: 'Windstorm' }
+        }
+    },
+    setup(props, ctx){
+        const capitalHeroName = computed(() => {
+            return props.hero.name.toUpperCase();
+        });
+
+        return { capitalHeroName }
+    }
+})
+</script>
